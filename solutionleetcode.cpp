8745fda94958c69a31a5779ab238cfaeaf805edb
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-16 17:42:18 +0200

    Update solutionleetcode.cpp

--- solutionleetcode.cpp
+++ solutionleetcode.cpp
@@ -3,7 +3,7 @@ public:
     int numberOfArrays(string s, int k) {  
         int n = s.size();   
         vector<int> dp(n+1);   
-        dp[n] = 1; 
+        dp[n] = 1;  
         for(int i = n-1; i >= 0; i--) {  
             if(s[i] == '0') continue; // leading zero not allowed
             long long num = 0; 
